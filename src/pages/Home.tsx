badd4a821f851c9730beddfe8931d04e6d391f86
Talha-on-Git <talhayousuf107@gmail.com>
2025-04-29 23:20:42 +0500

    Initial project setup with Tailwind, Redux, Router, etc.

---
+++ src/pages/Home.tsx
@@ -0,0 +1,16 @@
+
+
+const Home = () => {
+  return (
+    <div className="p-8">
+      <div className="container bg-white">
+        <h1 className="mb-6 text-3xl font-bold text-gray-800">
+          Featured Products
+        </h1>
+      </div>
+      <h1>DUMMY LINE Home</h1>
+    </div>
+  );
+};
+
+export default Home;
